d09c9a46b1aecefee392f469cd043fcf06f3a28f
SubhasishDutta-Coder <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2019-12-17 08:38:16 +0600

    Update positive number.c

--- positive number.c
+++ positive number.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
-int main()
-{ int a;
-printf("Enter the digit\n");
-scanf("%d",&a);
-{if(a>0||a==0)
-{printf("%d is a positive number\n",a);}
+int
+main ()
+{
+  int a;
+  printf ("Enter the digit\n");
+  scanf ("%d", &a);
+  {
+    if (a > 0 || a == 0)
+      {
+	printf ("%d is a positive number\n", a);
+      }
 
-else
-{printf("%d is not a positive number\n",a);}
+    else
+      {
+	printf ("%d is not a positive number", a);
+      }
+  }
+
+  return 0;
+
 }
 
-return 0;
-
-}

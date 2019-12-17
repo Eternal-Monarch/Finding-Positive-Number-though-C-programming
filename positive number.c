810373e70a3a633a059d596bb73c0b9dc9326d85
SubhasishDutta-Coder <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2019-12-17 08:37:48 +0600

    Add files via upload

---
+++ positive number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ int a;
+printf("Enter the digit\n");
+scanf("%d",&a);
+{if(a>0||a==0)
+{printf("%d is a positive number\n",a);}
+
+else
+{printf("%d is not a positive number\n",a);}
+}
+
+return 0;
+
+}
